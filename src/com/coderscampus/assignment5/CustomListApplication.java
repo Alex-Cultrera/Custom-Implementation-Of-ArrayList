da1494dfe7f413b6a7da563d22536c18702a49fc
Alex-Cultrera <alexander.cultrera@gmail.com>
2024-07-02 12:30:13 -0400

    9th commit

--- src/com/coderscampus/assignment5/CustomListApplication.java
+++ src/com/coderscampus/assignment5/CustomListApplication.java
@@ -6,8 +6,7 @@ public class CustomListApplication {
 
 		// ***TESTING PROCESS***
 
-		// test by instantiating the CustomArrayList class and inserting elements into
-		// it
+		// test by instantiating the CustomArrayList class and inserting elements into it
 		CustomList<String> myCustomList = new CustomArrayList<>();
 		myCustomList.add("element 1");
 		// instantiation of additional CustomArrayList for further testing
@@ -17,12 +16,11 @@ public class CustomListApplication {
 		myShoppingList.add("milk");
 
 		// and continue to add another 10, 20 or 40 more elements to myCustomList
-		// method to quickly add multiple elements to the CustomArrayList
+		// method below will quickly add specified number elements to the CustomArrayList
 		Elements elements = new Elements();
 		elements.addElementsToCustomList(10, myCustomList);
 
-		// then validate that all the elements inserted actually exist in the data
-		// structure
+		// then validate that all the elements inserted actually exist in the data structure
 		for (int i = 0; i < myCustomList.getSize(); i++) {
 			System.out.println(myCustomList.get(i));
 		}
